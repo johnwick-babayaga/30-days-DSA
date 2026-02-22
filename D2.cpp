4112f89c2731b477eb309b2d7d8ebc6561a4f44d
johnwick-babayaga <akankitbgs777@gmail.com>
2026-02-23 00:10:58 +0530

    day2

---
+++ D2.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printVector(vector<int> nums)
+{
+    int n = nums.size();
+    for (int i = 0; i < n; i++)
+    {
+        cout << nums[i] << " ";
+    }
+}
+void printMatrix(vector<vector<int>> &matrix)
+{
+    int n = matrix.size(), m = matrix[0].size();
+    for (int i = 0; i < n; i++)
+    {
+        cout << "[";
+        for (int j = 0; j < m; j++)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        cout << "]";
+        cout << endl;
+    }
+}
+
+// BRUTE APPROACH- SET ZEROES
+void setZeroes(vector<vector<int>> &matrix)
+{
+    int n = matrix.size(), m = matrix[0].size();
+    auto mRows = [&](int i)
+    {
+        for (int k = 0; k < m; k++)
+        {
+            if (matrix[i][k] != 0)
+            {
+                matrix[i][k] = -1;
+            }
+        }
+    };
+    auto mColumns = [&](int j)
+    {
+        for (int k = 0; k < n; k++)
+        {
+            if (matrix[k][j] != 0)
+            {
+                matrix[k][j] = -1;
+            }
+        }
+    };
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (matrix[i][j] == 0)
+            {
+                mRows(i);
+                mColumns(j);
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (matrix[i][j] == -1)
+            {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+}
+// BETTER APPROACH
+void setMatrixZeroes(vector<vector<int>> &matrix)
+{
+    int n = matrix.size(), m = matrix[0].size();
+    vector<int> row(n, 0);
+    vector<int> col(m, 0);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (matrix[i][j] == 0)
+            {
+                col[j] = 1;
+                row[i] = 1;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (row[i] || col[j])
+            {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> matrix(n, vector<int>(m));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> matrix[i][j];
+        }
+    }
+    printMatrix(matrix);
+    cout << endl;
+    // setZeroes(matrix);
+    // setMatrixZeroes(matrix);
+    printMatrix(matrix);
+}
