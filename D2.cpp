69d8a0e2aa89b4bb0e2252e217630c3d6c9af5d5
johnwick-babayaga <akankitbgs777@gmail.com>
2026-02-23 00:46:33 +0530

    changes

--- D2.cpp
+++ D2.cpp
@@ -74,8 +74,8 @@ void setZeroes(vector<vector<int>> &matrix)
 void setMatrixZeroes(vector<vector<int>> &matrix)
 {
     int n = matrix.size(), m = matrix[0].size();
-    vector<int> row(n, 0);
-    vector<int> col(m, 0);
+    vector<int> row(n, 0); //matrix[..][0]
+    vector<int> col(m, 0); //matrix[0][..]
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
@@ -99,21 +99,68 @@ void setMatrixZeroes(vector<vector<int>> &matrix)
     }
 }
 
-int main()
-{
-    int n, m;
-    cin >> n >> m;
-    vector<vector<int>> matrix(n, vector<int>(m));
-    for (int i = 0; i < n; i++)
+void optimalSetZeores(vector<vector<int>> &matrix){
+    int  n = matrix.size(), m = matrix[0].size();
+    int col0 = 1;
+    for(int i = 0; i< n; i++){
+        for(int j=0; j< m; j++){
+            if(matrix[i][j] == 0){
+                //mark i-th row as zero
+                matrix[i][0] = 0;
+                //mark j-th column as zero
+                if(j != 0){
+                    matrix[0][j] = 0;
+                }
+                else{
+                    col0 = 0;
+                }
+            }
+        }
+    }
+    for (int i = 1; i < n; i++){
+        for (int j = 1; j < m; j++){
+            if(matrix[i][j] != 0){
+                if( matrix[i][0] == 0 || matrix[0][j] == 0){
+                matrix[i][j] = 0;
+            }
+        }
+        }
+    }
+    if (matrix[0][0] == 0)
     {
         for (int j = 0; j < m; j++)
         {
-            cin >> matrix[i][j];
+            matrix[0][j] = 0;
         }
     }
-    printMatrix(matrix);
-    cout << endl;
-    // setZeroes(matrix);
-    // setMatrixZeroes(matrix);
-    printMatrix(matrix);
+    if (col0 == 0)
+    {
+        for(int i = 0; i < n; i++){
+            matrix[i][0] = 0;
+        }
+    }
+   
 }
+
+
+
+
+        int main()
+        {
+            int n, m;
+            cin >> n >> m;
+            vector<vector<int>> matrix(n, vector<int>(m));
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    cin >> matrix[i][j];
+                }
+            }
+            printMatrix(matrix);
+            cout << endl;
+            // setZeroes(matrix);
+            // setMatrixZeroes(matrix);
+            optimalSetZeores(matrix);
+            printMatrix(matrix);
+        }
